dac6deaaa4a4abb9a6c64573055bdd7f4feb3e36
Kristián Kostecký <kostecky.kristian@gmail.com>
2023-02-26 11:06:55 +0100

    Add user model to prisma

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,21 +1,30 @@
 import { useLoaderData } from '@remix-run/react'
+import { prisma } from '~/db.server'
 
-export function loader() {
-  return [
-    {
-      answer: 'This is answer for this question',
-      question: 'This is content of the question, asking you...?',
-      title: 'This is first question',
-    },
-  ]
+export async function loader() {
+  const cards = await prisma.card.findMany({
+    select: { answer: true, question: true },
+  })
+  return { cards }
 }
 
 export default function Index() {
-  const data = useLoaderData()
+  const { cards } = useLoaderData<typeof loader>()
   return (
     <main className="text-base">
-      <h1 className="text-xl">Learn Loop</h1>
-      <code>{JSON.stringify(data, null, 2)}</code>
+      <h1 className="mb-4 text-center text-xl">Learn Loop</h1>
+      <div className="mx-auto max-w-lg">
+        <div className="mb-2 flex justify-between border-b-2 ">
+          <div className="mr-1 font-medium">Question</div>
+          <div className="font-medium">Answer</div>
+        </div>
+        {cards.map((card) => (
+          <div className="mb-2 flex justify-between ">
+            <div className="mr-1">{card.question}</div>
+            <div>{card.answer} </div>
+          </div>
+        ))}
+      </div>
     </main>
   )
 }
---
+++ prisma/seed.ts
@@ -0,0 +1,40 @@
+import type { Prisma } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt'
+
+const prisma = new PrismaClient()
+
+const getCards = (userId: string): Array<Prisma.CardCreateArgs['data']> => {
+  return [
+    {
+      answer: '118',
+      question: 'How many elements are there in the periodic table',
+      userId,
+    },
+    {
+      answer: 'Saffron',
+      question: 'What is the most expensive spice in the world?',
+      userId,
+    },
+    {
+      answer: `Vatican City`,
+      question: 'What is the smallest country in the world?',
+      userId,
+    },
+  ]
+}
+
+async function seed() {
+  const hash = await bcrypt.hash('password', 10)
+  const user = await prisma.user.create({
+    data: { password: { create: { hash } }, username: 'john' },
+  })
+
+  await Promise.all(
+    getCards(user.id).map((card) => {
+      return prisma.card.create({ data: card })
+    })
+  )
+}
+
+void seed()
